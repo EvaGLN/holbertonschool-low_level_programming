c7ba2092348209819b411a3acc602d8d1088f38b
EvaGLN <maeva.galiano@gmail.com>
2026-06-22 13:57:03 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/**
+/*
  * Print "Programming is like building a multilingual puzzle
  * Followed by a new line
  * Use the function puts
  * You are not allowed to use printf
- * Your program should end with the value 0*/
+ * Your program should end with the value 0
+ */
 
 int main(void)
 {
