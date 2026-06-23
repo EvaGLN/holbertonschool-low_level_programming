2c9525a86fbb0f13555b7d9768d9677c87b263ce
EvaGLN <maeva.galiano@gmail.com>
2026-06-23 22:20:11 +0200

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - check lowercase character
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	char c
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
