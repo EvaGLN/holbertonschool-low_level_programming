809951f81d005ffdb6f63866358855a86fe061b2
EvaGLN <maeva.galiano@gmail.com>
2026-06-23 10:43:03 +0200

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print 1 to 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+	for (number = 0; number < 10; number++)
+	{
+		printf(number);
+	}
+	printf('\n');
+	return (0)
+}
+
