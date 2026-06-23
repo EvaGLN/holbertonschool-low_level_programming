72e2ccd7c1ec54c9571f2add87f2677dc47f3cbe
EvaGLN <maeva.galiano@gmail.com>
2026-06-23 10:18:35 +0200

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet without e and q
+ * Return: Always 0
+ */
+int main(void)
+{
+	char minuscule;
+
+	for (minuscule = 'a'; minuscule <= 'z'; minuscule++)
+	{
+		if (minuscule != 'e' && minuscule != 'q')
+		{
+		putchar(minuscule);
+		}
+		minuscule++;
+	}
+	putchar('\n');
+	return (0);
+}
