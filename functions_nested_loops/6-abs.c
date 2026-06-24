24a5af14607e8b180effb10f963965f16f4cf06b
EvaGLN <maeva.galiano@gmail.com>
2026-06-24 08:27:01 +0200

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute value of int
+ * @r: to check
+ * Return: Always 0
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+	return (0);
+}
