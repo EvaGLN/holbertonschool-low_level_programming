e8432f81bb8ec745497bb90df26ad9219138ca08
EvaGLN <maeva.galiano@gmail.com>
2026-06-25 10:33:42 +0200

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - obvious
+ * @size: limit of the triangle
+ */
+void print_triangle(int size)
+{
+	int l;
+	int e;
+	int h;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 0; l < size; l++)
+		{
+			for (e = 0; e < (size - l - 1); e++)
+			{
+				_putchar(' ');
+			}
+			for (h = 0; h < (l + 1); h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n')
+		}
+	}
+}
