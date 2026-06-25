e117498f274542b8c078cbf3fa116a9437554a76
EvaGLN <maeva.galiano@gmail.com>
2026-06-25 08:50:55 +0200

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - obvious
+ * @size: size of the square
+ */
+void print_quare(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (y = 1; y <= size; y++)
+		{
+			for (x = 1; x <= y; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
