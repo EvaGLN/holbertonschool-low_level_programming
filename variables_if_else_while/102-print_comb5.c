5e1aabad656a14b057c84c4b71c37edbe1838703
EvaGLN <maeva.galiano@gmail.com>
2026-06-26 11:02:12 +0200

    102-print_comb5.c

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -13,9 +13,9 @@ int main(void)
 		{
 			for (d = 0; d < 10; d++)
 			{
-				for(u = 0; u < 10; u++)
+				for (u = 0; u < 10; u++)
 				{
-					if (d > m || (d == m && u> c))
+					if (d > m || (d == m && u > c))
 					{
 						putchar(m + '0');
 						putchar(c + '0');
