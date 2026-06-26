8a620e7a9b126506725d2e338fd44c5e7db3f67c
EvaGLN <maeva.galiano@gmail.com>
2026-06-26 10:00:22 +0200

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 
 	for (d = 0; d < 10; d++)
 	{
-		for (u = 1; u < 10; u++)
+		for (u = d + 1; u < 10; u++)
 		{
 			putchar(d + '0');
 			putchar(u + '0');
-			if (d < 8 && u < 9)
+			if (d != 8 && u != 9)
 			{
 				putchar(',');
 				putchar(' ');
