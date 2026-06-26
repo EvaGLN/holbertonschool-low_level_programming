20b0fcd2b8b6c90c5dda0f520656d89fbcac2c58
EvaGLN <maeva.galiano@gmail.com>
2026-06-26 09:31:56 +0200

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
- * main - 
+ * main - print smallest combinations of 2 digits except doubles
+ * Return: Always 0
  */
 int main(void)
 {
 	int d;
 	int u;
 
-	if (d < u)
+	for (d = 0; d < 10; d++)
 	{
-		for (d = 0; d < 10; d++)
+		for (u = 0; u < 10; u++)
 		{
-			for (u = 0; u < 10; u++)
+			if (d != u)
 			{
 				putchar(d + '0');
 				putchar(u + '0');
