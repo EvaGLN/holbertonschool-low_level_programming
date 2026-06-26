ec8cc4d0804fde4f38c6dbc4e541557474640aa7
EvaGLN <maeva.galiano@gmail.com>
2026-06-26 12:49:36 +0200

    .

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_times_table - time tables until square no more than 15
+ * @n: to multiply
+ */
+void print_times_table(int n)
+{
+	int line;
+	int column;
+	int result;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	for (line = 0; line <= n; line++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			result = (line * column);
+			if (column  == 0)
+			{
+				_putchar(result + '0');
+			}
+			else if (result <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
