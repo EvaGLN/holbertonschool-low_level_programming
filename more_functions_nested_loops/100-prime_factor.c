ceb5ceccdabbd37d427cd9991b5634b99a881fdd
EvaGLN <maeva.galiano@gmail.com>
2026-06-27 10:44:24 +0200

    .

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -5,22 +5,22 @@
 */
 int main(void)
 {
-    unsigned long limit, attempt, largest;
-
-    limit = 612852475143UL;
-    largest = 1;
-    for (attempt = 2; attempt <= limit; attempt++)
-    {
-        for (; limit % attempt == 0;)
-        {
-            largest = attempt;
-            limit = limit / attempt;
-        }
-    }
-    if (limit > 1)
-    {
-        largest = limit;
-    }
-    printf("%lu\n", largest);
-    return (0);
+	unsigned long limit, attempt, largest;
+	
+	limit = 612852475143UL;
+	largest = 1;
+	for (attempt = 2; attempt <= limit; attempt++)
+	{
+		for (; limit % attempt == 0;)
+		{
+			largest = attempt;
+			limit = limit / attempt;
+		}
+	}
+	if (limit > 1)
+	{
+		largest = limit;
+	}
+	printf("%lu\n", largest);
+	return (0);
 }
