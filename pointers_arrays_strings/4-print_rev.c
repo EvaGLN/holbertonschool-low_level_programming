e588e2af47b544f7d1bae629e3ca3d86602fde8a
EvaGLN <maeva.galiano@gmail.com>
2026-06-30 13:05:56 +0200

    .

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
+ * print_rev - prints a string, in reverse, followed by a new line
  * @s: pointer to a char
  */
 void print_rev(char *s)
 {
 	int caract;
 	int lenght;
-	
-	for (lenght = 0; s[lenght] != '\0'; lenght++)
+
+	for (lenght = 0 s[lenght] != '\0'; lenght++)
 	{
 	}
 	for (caract = lenght - 1; caract >= 0; caract--)
