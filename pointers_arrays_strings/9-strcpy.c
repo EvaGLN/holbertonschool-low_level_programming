21f4cf58be0764eb597bbf3152a71852f4c49751
EvaGLN <maeva.galiano@gmail.com>
2026-06-30 16:18:26 +0200

    .

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copy str pointed to by src include term \0 to buffer pointed to by dest
+ * @dest: destination buffer
+ * @src: source of the string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int caract;
+
+	for (caract = 0; src[caract] != '\0'; caract++)
+	{
+		dest[caract] = src[caract];
+	}
+	dest[caract] = '\0';
+	return(dest);
+}
