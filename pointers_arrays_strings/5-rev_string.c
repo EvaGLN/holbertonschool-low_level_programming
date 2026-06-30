13165cc4f62b6389e14c30ca9ab7e5a3261426b2
EvaGLN <maeva.galiano@gmail.com>
2026-06-30 14:19:16 +0200

    .

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,16 @@
-#include "main.h"
+i#include "main.h"
 /**
  * rev_string - reverse a string
  * @s: pointer to a char
  */
 void rev_string(char *s)
 {
-	int a, b, lenght, tmp; 
-	
-	for (lenght = 0; s[lenght] != '\0'; lenght++)
+	int a, b, lenght, tmp;
+
+	for (lenght = 0, s[lenght] != '\0'; lenght++)
 	{
 	}
-	for (a = 0, b = lenght - 1; a < b; a++, b--)
+	for (a = 0, b = lenght -1; a < b; a++, b--)
 	{
 		tmp = s[a];
 		s[a] = s[b];
