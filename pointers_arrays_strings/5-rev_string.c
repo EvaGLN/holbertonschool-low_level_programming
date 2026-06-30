9d6d7b7f7fc91e87b6073469225b4dd72a1f097d
EvaGLN <maeva.galiano@gmail.com>
2026-06-30 14:11:10 +0200

    .

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,15 +4,15 @@
  */
 void rev_string(char *s)
 {
-	int a, *a, b, *b, lenght, tmp;
+	int a, b, lenght, tmp;
 	
 	for (lenght = 0; s[lenght] != '\0'; lenght++)
 	{
 	}
 	for (a = 0, b = lenght - 1; a < b; a++, b--)
 	{
-		tmp = *a;
-		*a = *b;
-		*b = tmp;
+		tmp = s[a];
+		s[a] = s[b];
+		s[b] = tmp;
 	}
 }
