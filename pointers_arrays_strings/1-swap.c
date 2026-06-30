64ad7e2c758e3d2228e8ec0861c60cb20c542208
EvaGLN <maeva.galiano@gmail.com>
2026-06-30 09:45:10 +0200

    .

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer a
+ * @b: pointer b
+ */
+void swap_int(int *a, int *b);
+{
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
