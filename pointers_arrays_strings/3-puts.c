57165f5deee0198c87a3e9d2debcfd340ed16c8e
EvaGLN <maeva.galiano@gmail.com>
2026-06-30 12:15:10 +0200

    .

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -7,9 +7,9 @@ void _puts(char *str)
 {
 	int caract;
 
-	for (caract = '0'; str[caract] != '\0'; caract++)
+	for (caract = 0; str[caract] != '\0'; caract++)
 	{
-		_putchar(caract);
+		_putchar(caract + '0');
 	}
 	_putchar('\n');
 }
