2bc8232305df90e3237859e0b0b3616096cceb87
EvaGLN <maeva.galiano@gmail.com>
2026-07-01 17:02:50 +0200

    .

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode a string to L337
+ * @:
+ * Return: 
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char *caract = "aAeEoOtTlL";
+	char *change = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; caract[j] != '\0'; j++)
+		{
+			if (s[i] == caract[j])
+			{
+				s[i] = change[j];
+			}
+		}
+	}
+	return (s);
+}
