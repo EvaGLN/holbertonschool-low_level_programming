9914bc7004f04a32e34b65a63669bf2ee5aadeef
EvaGLN <maeva.galiano@gmail.com>
2026-07-01 11:52:40 +0200

    .

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: result of comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; *s1 != '\0' && *s2 != '\0'; i++, s1++, s2++)
+	{
+	}
+	return (s1 - s2);
+}
