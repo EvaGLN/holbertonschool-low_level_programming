3f75632f097446a3ced7623b17e6ec8cbaf12272
EvaGLN <maeva.galiano@gmail.com>
2026-07-01 10:30:03 +0200

    .

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings could be limited by n
+ * @dest: begining of the string
+ * @src: end of the string
+ * @n: limit of copy
+ * Retrun: point to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr;
+	int i;
+
+	for (ptr = dest; *ptr != '\0'; ptr++)
+	{
+	}
+	for (i = 0; i < n && *src != '\0'; i++, ptr++, src++)
+	{
+		*ptr = *src;
+	}
+	*ptr = '\0';
+	return (dest);
+}
