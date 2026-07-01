3edd10ef267ca2780e7f4d37009f3337438b1fc6
EvaGLN <maeva.galiano@gmail.com>
2026-07-01 11:54:01 +0200

    .

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; *s1 != '\0' && *s2 != '\0'; i++, s1++, s2++)
+	for (i = 0; *s1 != '\0' || *s2 != '\0'; i++, s1++, s2++)
 	{
 	}
 	return (s1 - s2);
