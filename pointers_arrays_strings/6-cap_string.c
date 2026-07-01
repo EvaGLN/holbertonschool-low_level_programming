657be27eade2a8265ace845ef6edb75c80f2fd33
EvaGLN <maeva.galiano@gmail.com>
2026-07-01 15:03:48 +0200

    .

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - capitalize the first letter of all words
+ * @s: pointer to a char
+ * Return: string modified
+ */
+char *cap_string(char *s)
+{
+	int n, o;
+	int decode = 0;
+	char *sep = " \t\n,;.!?\"(){}";
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		for (o = 0; sep[o] != '\0' && s[n] != sep[o]; o++)
+		{
+			if (sep[o] != '\0')
+			{
+				decode = 0;
+			}
+			else if (s[n] >= 'a' && s[n] <= 'z' && decode == 0)
+			{
+				s[n] = s[n] - ('a' - 'A');
+				decode = 1;
+			}
+			else
+			{
+				decode = 1;
+			}
+		}
+	}
+	return (s);
+}
