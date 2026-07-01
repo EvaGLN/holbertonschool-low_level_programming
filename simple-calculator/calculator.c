6d47814bfcc915c01c38f0bf56acac6ce7450e82
EvaGLN <maeva.galiano@gmail.com>
2026-07-01 20:36:53 +0200

    .

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -16,53 +16,51 @@ int main ()
 	{
 		printf("Simple Calculator\n1) Add\n2) Substract\n");
 		printf("3) Multiply\n4) Divide\n0) Quit\n");
-
 		scanf("%d\n", &choice);
 
-		if (choice >= 0 && choice <= 4)
-			if (choice == 1)
-			{
-				printf("What's you first number to add?\n");
-				scanf("%d\n", &A);
-				printf("What's your second number to add?\n");
-				scanf("%d\n", &B);
-				result = A + B;
-				printf("Result: %d\n", result);
-			}
-			else if (choice == 2)
-			{
-				printf("What's you first number to substract?\n");
-				scanf("%d\n", &A);
-				printf("What's your second number to substract?\n");
-				scanf("%d\n", &B);
-				result = A - B;
-				printf("Result: %d\n", result);
-			}
-			else if (choice == 3)
-			{
-				printf("What's you first number to multiply?\n");
-				scanf("%d\n", &A);
-				printf("What's your second number to multiply?\n");
-				B = int scanf("%d\n", &B);
-				result = A * B;
-				printf("Result: %d\n", result);
-			}
-			else if (choice == 4)
-			{
-				printf("What's you first number to divide?\n");
-				scanf("%d\n", &A);
-				printf("What's your second number to divide?\n");
-				scanf("%d\n", &B);
-				result = A / B;
-				printf("Result: %d\n", result);
-			}
-			else if (choice == 0)
-			{
-				printf("Bye!\n");
-			}
-			else
-			{
-				printf("Invalid choice\n");
-			}
+		if (choice == 0)
+		{
+			printf("Bye!\n");
+		}
+		else if	(choice == 1)
+		{
+			printf("What's you first number to add?\n");
+			scanf("%d\n", &A);
+			printf("What's your second number to add?\n");
+			scanf("%d\n", &B);
+			result = A + B;
+			printf("Result: %d\n", result);
+		}
+		else if (choice == 2)
+		{
+			printf("What's you first number to substract?\n");
+			scanf("%d\n", &A);
+			printf("What's your second number to substract?\n");
+			scanf("%d\n", &B);
+			result = A - B;
+			printf("Result: %d\n", result);
+		}
+		else if (choice == 3)
+		{
+			printf("What's you first number to multiply?\n");
+			scanf("%d\n", &A);
+			printf("What's your second number to multiply?\n");
+			B = int scanf("%d\n", &B);
+			result = A * B;
+			printf("Result: %d\n", result);
+		}
+		else if (choice == 4)
+		{
+			printf("What's you first number to divide?\n");
+			scanf("%d\n", &A);
+			printf("What's your second number to divide?\n");
+			scanf("%d\n", &B);
+			result = A / B;
+			printf("Result: %d\n", result);
+		}
+		else
+		{
+			printf("Invalid choice\n");
+		}
 	}
 }
