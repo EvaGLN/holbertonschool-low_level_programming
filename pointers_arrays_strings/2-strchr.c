f907509ee1f92225b39fca641baa94b642bb44e1
EvaGLN <maeva.galiano@gmail.com>
2026-07-02 08:43:06 +0200

    .

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,18 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (;*s != '\0'; s++)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (s[i]);
+			return (s);
 		}
 	}
 	if (c == '\0')
 	{
-		return (s[i]);
+		return (s);
 	}
 	return (s);
 }
