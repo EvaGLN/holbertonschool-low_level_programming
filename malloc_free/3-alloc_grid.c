1a181a76bfb933c8db184094eb924a021241e3c8
EvaGLN <maeva.galiano@gmail.com>
2026-07-06 15:46:38 +0200

    .

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - turn a pointer to 2D array of int
+ * @width: width of grid
+ * @height: height of grid
+ * Return: pointer to array of NULL if fail
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, *a;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			while (i > 0; i--)
+			{
+				free(a[i]);
+			}
+			free(a);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
+}
