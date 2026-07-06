442e544098da3bc64486d1f60eb05f87aac8172a
EvaGLN <maeva.galiano@gmail.com>
2026-07-06 14:15:22 +0200

    .

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,10 @@
+#include "main."
+#include <stdlib.h>
+/**
+ * str_concat - concatenate 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL if fail
+ */
+char *str_concat(char *s1, char *s2)
+
