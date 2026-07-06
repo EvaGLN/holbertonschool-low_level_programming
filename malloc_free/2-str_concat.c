7b994911f72ab453da64d58f95e7196789c521d7
EvaGLN <maeva.galiano@gmail.com>
2026-07-06 15:19:18 +0200

    .

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,7 +4,40 @@
  * str_concat - concatenate 2 strings
  * @s1: string 1
  * @s2: string 2
- * Return: NULL if fail
+ * Return: pointer to newly allocated space or NULL if fail
  */
 char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *a;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i]; i++)
+	{
+	}
+	for (j = 0; s2 [j]; j++)
+	{
+	}
+	a = malloc((i+j+1) * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		a[i] = s1[i];
+	}
+	for (j = 0; a[j]; j++)
+	{
+		a[i] = s2[j]
+	}
+	a[i] = '\0';
+	return (a);
+}
