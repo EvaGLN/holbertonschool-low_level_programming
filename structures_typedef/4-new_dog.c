fc781a1031fbc16a86016a715ddefabee4165b10
EvaGLN <maeva.galiano@gmail.com>
2026-07-08 08:49:57 +0200

    .

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *copy_name, *copy_owner;
+	int copy_name, copy_owner, i;
 	dog_t *my_dog;
 
 	for (copy_name = 0; name[copy_name]; copy_name++)
@@ -24,18 +24,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	my_dog->name = malloc(copy_name);
+	my_dog->name = malloc(copy_name + 1);
 	if (my_dog->name == NULL)
 	{
+		free(my_dog);
 		return (NULL);
 	}
-	my_dog->owner = malloc(copy_owner);
+	my_dog->owner = malloc(copy_owner + 1);
 	if (my_dog->owner == NULL)
 	{
+		free(my_dog->name);
+		free(my_dog);
 		return (NULL);
 	}
-	my_dog->name = name;
+	for (i = 0; i < copy_name; i++)
+	{
+		my_dog->name[i] = name[i];
+	}
+	my_dog->name[cppy_name] = '\0';
 	my_dog->age = age;
-	my_dog->owner = owner;
+	for (i = 0 < copy_owner; i++)
+	{
+		my_dog->owner[i] = owner[i];
+	}
+	my_dog->owner[copy_owner] = '\0';
 	return (my_dog);
 }
