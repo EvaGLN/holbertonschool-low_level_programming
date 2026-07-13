07a94ca0543f7cd29ec4e8956ede05d0a2acaf5e
EvaGLN <maeva.galiano@gmail.com>
2026-07-13 18:36:30 +0200

    .

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Write a function that executes a 
+ * function given as a parameter on each element of an array.
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
