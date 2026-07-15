2449f41a8cac9d1ef18c8e0f4b8c5ada1b013fb0
EvaGLN <maeva.galiano@gmail.com>
2026-07-15 10:43:39 +0200

    .

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Write a program that prints the number of arguments passed into it.
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv[];
+	printf("%d", argc);
+	return (0);
+:x
