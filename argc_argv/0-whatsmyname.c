7005d4559a1c3657ee8f3de6ac9565d3cd146e64
EvaGLN <maeva.galiano@gmail.com>
2026-07-15 10:33:54 +0200

    .

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * @argc: count of the arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
