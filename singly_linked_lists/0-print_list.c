16cd0c4f88db5b33ffe6276a919a7d41b38e95f5
EvaGLN <maeva.galiano@gmail.com>
2026-07-21 08:48:43 +0200

    task 0 - 3rd Betty Style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,13 +11,9 @@ size_t print_list(const list_t *h)
 	for (count = 0; h != NULL; count++, h = h->next)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 	}
 	return (count);
 }
