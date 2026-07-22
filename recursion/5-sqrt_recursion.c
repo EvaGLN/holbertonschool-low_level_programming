15cf192d261b490b3d49a5353e0c1b3a1f3663d9
EvaGLN <maeva.galiano@gmail.com>
2026-07-22 08:59:00 +0200

    task 5 - 1st

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_finder - find square root of number
+ * if it exists
+ * @n: base
+ * @i: tester
+ * Return: natural square root or -1 if number doesn't have one
+ */
+int _sqrt_finder(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (_sqrt-analysis(n, i + 1));
+}
+/**
+ * _sqrt_recursion - Write a function that returns
+ * the natural square root of a number.
+ * @n: number
+ * Return: natural square root or -1 if number doesn't have one
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_analysis(n, 0));
+}
