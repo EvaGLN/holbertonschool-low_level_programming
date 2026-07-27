584788c29180f28962cc53c8b4a1e6c8b5256e23
EvaGLN <maeva.galiano@gmail.com>
2026-07-27 14:09:55 +0200

    1st commit

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,6 +1,28 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ */
+typedef struct dlistint_s
+{
+	char *str;
+	unsigned int len;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 
 size_t dlistint_len(const dlistint_t *h);
