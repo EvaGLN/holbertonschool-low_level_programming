6eab9a2afe5c5794360386c32d594a72ae9bbb53
EvaGLN <maeva.galiano@gmail.com>
2026-07-28 10:36:07 +0200

    1st commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,3 +9,27 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new;
+	dlistint_t *prev_node;
+	size_t len;
+
+	len = dlistint_len(*h);
+	if (idx > len)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (idx == len)
+		return (add_dnodeint_end(h, n));
+	prev_node = get_dnodeint_at_index(*h, idx - 1);
+	if (prev_node == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = prev_node;
+	new->next = prev_node->next;
+	prev_node->next->prev = new;
+	prev_node->next = new;
+	return (new);
+}
