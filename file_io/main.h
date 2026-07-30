b2f538f4f1cd297df0dac7e128087eb6f47cd331
EvaGLN <maeva.galiano@gmail.com>
2026-07-30 12:21:23 +0200

    3rd commit

--- file_io/main.h
+++ file_io/main.h
@@ -15,8 +15,6 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
-void check_argc(int ac);
-
 int open_file_from(char *file_from);
 
 int open_file_to(char *file_to);
